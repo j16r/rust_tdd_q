471c45dde6af38a23c31e8381fe0277cd6617094
John Barker <jbarker@amplify.com>
2014-06-17 14:39:12 -0400

    Use Option for return types that could operate on an empty queue

--- q.rs
+++ q.rs
@@ -1,32 +1,38 @@
 
-struct Q {
-  items : Vec<int>,
+struct Q<T> {
+  items : Vec<T>,
   count : uint,
-  position : uint
+  position : uint,
+  limit : uint
 }
 
-impl Q {
-  pub fn new() -> ~Q {
+impl<T> Q<T> {
+  pub fn new() -> ~Q<T> {
     Q::with_size(10)
   }
 
-  pub fn with_size(size : uint) -> ~Q {
+  pub fn with_size(size : uint) -> ~Q<T> {
     ~Q {items: Vec::with_capacity(size),
         count: 0,
-        position: 0}
+        position: 0,
+        limit: size}
   }
 
   pub fn empty(&self) -> bool {
     self.size() == 0
   }
 
-  pub fn prepend(&mut self, value : int) {
+  pub fn prepend(&mut self, value : T) {
     self.items.insert(self.count, value);
     self.count += 1
   }
 
-  pub fn peek(&self) -> int {
-    *self.items.get(self.position)
+  pub fn peek(&self) -> Option<T> {
+    if self.empty() {
+      None
+    } else {
+      Some(*self.items.get(self.position))
+    }
   }
 
   pub fn remove(&mut self) {
@@ -36,18 +42,23 @@ impl Q {
   pub fn size(&self) -> uint {
     self.count - self.position
   }
+
+  pub fn full(&self) -> bool {
+    self.size() == self.limit
+  }
 }
+
 
 #[test]
 fn test_empty() {
-  let q = Q::new();
+  let q = Q::<int>::new();
 
   assert!(q.empty() == true);
 }
 
 #[test]
 fn test_prepend() {
-  let mut q = Q::new();
+  let mut q = Q::<int>::new();
 
   q.prepend(99);
 
@@ -67,8 +78,17 @@ fn test_prepend_with_two_insertions() {
 }
 
 #[test]
+fn test_prepend_with_all_the_things() {
+  let mut q = Q::<int>::with_size(1);
+
+  q.prepend(99);
+
+  assert!(q.full() == true);
+}
+
+#[test]
 fn test_prepend_with_too_many_things() {
-  let mut q = Q::with_size(1);
+  let mut q = Q::<int>::with_size(1);
 
   q.prepend(99);
   q.prepend(113);
@@ -81,7 +101,7 @@ fn test_peek() {
 
   q.prepend(99);
 
-  assert!(q.peek() == 99);
+  assert!(q.peek().unwrap() == 99);
 }
 
 #[test]
@@ -92,7 +112,14 @@ fn test_peek_with_removal() {
   q.remove();
   q.prepend(113);
 
-  assert!(q.peek() == 113);
+  assert!(q.peek().unwrap() == 113);
+}
+
+#[test]
+fn test_peek_on_empty_queue_returns_none() {
+  let q = Q::<int>::new();
+
+  assert!(q.peek().is_none());
 }
 
 #[test]
@@ -100,9 +127,9 @@ fn test_peek_with_a_few_things() {
   let mut q = Q::new();
 
   q.prepend(99);
-  assert!(q.peek() == 99);
+  assert!(q.peek().unwrap() == 99);
   q.prepend(113);
-  assert!(q.peek() == 99);
+  assert!(q.peek().unwrap() == 99);
   q.remove();
-  assert!(q.peek() == 113);
+  assert!(q.peek().unwrap() == 113);
 }
